6a4230548131241d6e24e46fce39b72a4991bcd8
张平 <447197782@qq.com>
2020-12-21 16:04:54 +0800

    实体类对象

---
+++ gmall-bean/src/main/java/com/ping/gmall/bean/SkuLsAttrValue.java
@@ -0,0 +1,9 @@
+package com.ping.gmall.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class SkuLsAttrValue implements Serializable{
+    String valueId;
+}
---
+++ gmall-bean/src/main/java/com/ping/gmall/bean/SkuLsResult.java
@@ -0,0 +1,20 @@
+package com.ping.gmall.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class SkuLsResult implements Serializable{
+
+
+    List<SkuLsInfo> skuLsInfoList;
+
+    long total;
+
+    long totalPages;
+
+    List<String> attrValueIdList;
+
+}
